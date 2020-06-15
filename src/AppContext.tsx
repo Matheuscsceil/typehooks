5773de8abcbd9f886de553e0891a13a20fe98771
Matheuscsceil <math.cs.ceil@gmail.com>
2020-06-15 02:07:27 -0300

    Inicial

---
+++ src/AppContext.tsx
@@ -0,0 +1,37 @@
+import React,{useContext,useReducer} from 'react';
+//import CartContext from './CartContext';
+
+interface Cart {
+    products: string[];
+    shipping_value?: number;
+}
+
+type CartActionType = {
+    type: 'ADD_PRODUCT' | 'REMOVE_PRODUCT'
+}
+
+const AppContext: React.FC = () => {
+    /* const {products} = useContext(CartContext); */
+
+    const cart = useReducer(
+        (state: Cart,action: CartActionType) => {
+            switch(action.type){
+                case 'ADD_PRODUCT':
+                    return {
+                        ...state,
+                        products: [...state.products, "Produto novo"]
+                    }
+                default: 
+                    return state;
+            }
+        },
+        {
+            products: [],
+            shipping_value: 0
+        }
+    )
+
+    return <div />;
+}
+
+export default AppContext;
